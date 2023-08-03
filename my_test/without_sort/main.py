d291600bb9865d0d011b94372083f433454e7cc0
Mackson Mesquita <129464904+MacksonMesquita@users.noreply.github.com>
2023-08-03 18:59:35 -0300

    Update main.py

--- my_test/without_sort/main.py
+++ my_test/without_sort/main.py
@@ -4,7 +4,7 @@ numbers = [
    5,3,2,1,9,7,10,34,67,99,88,
    77,66,89,100,20,21,69,12,4]
 
-for x in range(len(numbers)-1): # pela length de produtos -1 produto atual
+for x in range(len(numbers)-1): # pela length de produtos -1 produto atual (by length of products -1 current product)
     for y in range(len(numbers)-x-1):
       if numbers[y] > numbers[y+1]: 
         temp = numbers[y]
